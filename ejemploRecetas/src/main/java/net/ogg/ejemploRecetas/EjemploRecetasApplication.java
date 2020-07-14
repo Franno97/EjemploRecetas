bd5920c43c9be58b5c12fde67fdcae8b2eb8a46c
franj <franj@LAPTOP-DIRSTBTR>
2020-07-14 16:42:33 -0500

    Commit Receta

---
+++ ejemploRecetas/src/main/java/net/ogg/ejemploRecetas/EjemploRecetasApplication.java
@@ -0,0 +1,13 @@
+package net.ogg.ejemploRecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploRecetasApplication.class, args);
+	}
+
+}
---
+++ ejemploRecetas/src/test/java/net/ogg/ejemploRecetas/EjemploRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ogg.ejemploRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
